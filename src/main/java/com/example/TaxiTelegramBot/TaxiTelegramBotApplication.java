bd7804f960f4ae597e905c10f7efc2b15f57367f
ilya <ilasglv@gmail.com>
2023-12-14 17:45:52 +0300

    All files commit

---
+++ src/main/java/com/example/TaxiTelegramBot/TaxiTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaxiTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiTelegramBotApplication.class, args);
+	}
+
+}
